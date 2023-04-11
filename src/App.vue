4ed2cb0594a1fea8abdd3f3236441e9a6065db82
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-04-11 10:36:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import {store} from "./store.js";
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+
+  components: {
+},
+
+  methods: {
+  },
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
